c280d516f232fb01c461e752f3da0146594c0cb4
AHMED <mo017886@gmail.com>
2024-07-02 04:08:38 +0300

    the first version of project

--- android/app/src/main/kotlin/com/example/mynewsapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mynewsapp/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.mynewsapp
 
-import io.flutter.embedding.android.FlutterActivity
+//mport io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+//class MainActivity: FlutterActivity()
